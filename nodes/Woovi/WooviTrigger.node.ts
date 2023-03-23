f3015185961854f6bd66e969115f3efe06e0f715
Fernando Silva <fernandonsilva16@gmail.com>
2023-03-23 13:01:23 -0300

    fix(baseurl): woovi.com (#22)

--- nodes/Woovi/WooviTrigger.node.ts
+++ nodes/Woovi/WooviTrigger.node.ts
@@ -32,7 +32,7 @@ export class WooviTrigger implements INodeType {
 			},
 		],
 		requestDefaults: {
-			baseURL: 'https://api.openpix.com.br/api',
+			baseURL: 'https://api.woovi.com/api',
 			headers: {
 				Accept: 'application/json',
 				'Content-Type': 'application/json',
