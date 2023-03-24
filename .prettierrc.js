686cf3d3a0e595438c027371ac5283f19493bf39
Fernando Silva <fernandonsilva16@gmail.com>
2023-03-24 18:48:25 -0300

    feat(lint-staged): add (#29)

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,5 @@
 module.exports = {
-  semi: true,
-  trailingComma: 'all',
-  bracketSpacing: true,
-  useTabs: true,
-  tabWidth: 2,
-  arrowParens: 'always',
-  singleQuote: true,
-  quoteProps: 'as-needed',
-  endOfLine: 'lf',
-  printWidth: 100,
+	trailingComma: 'all',
+	singleQuote: true,
+	semi: true,
 };
